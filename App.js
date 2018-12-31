acf283569ac9e6a02864894af089df532efd8e1c
fbrun <fabien.brun@kcitylabs.fr>
2018-12-31 14:43:14 +0100

    rmove unused code

--- App.js
+++ App.js
@@ -21,13 +21,6 @@ import { createStore } from 'redux'
 import {connect, Provider} from 'react-redux'
 
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 const sampleState = {
     todos: [{
         key: "0",
@@ -279,16 +272,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
         marginTop: 100
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+   
     completed: {
         textDecorationLine: 'line-through'
     }
